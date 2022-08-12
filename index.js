b8d7e67763c0fcc9841cc0c2df27d13a45dd0a4b
Elliot Mangini <elliot.mangini@gmail.com>
2022-08-12 16:23:37 -0600

    commit tree 483c038838cbcf8f9e7ee70b0261ee727e70bfd2

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Add your code here
+// formats our POST
+const postifyObject = (postObject) => {
+
+    return {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(postObject),
+    };
+};
+
+// push response to DOM
+const pushToDOM = (response) => {
+    console.log(response);
+    let newH = document.createElement('h1');
+    newH.innerHTML = response.id
+    document.querySelector('body').append(newH);
+
+}
+
+// takes the args and makes a call, pushing the 
+const submitData = (inputName, inputEmail) => {
+
+    let postObject = {
+        name: inputName,
+        email: inputEmail,
+      }
+
+  return fetch("http://localhost:3000/users", postifyObject(postObject))
+  .then(resp => resp.json())
+  .then(resp => pushToDOM(resp))
+  .catch(error => document.body.innerHTML = error);
+
+}
+ 
+
+// setTimeout(submitData('el', 'el@gmail.com'),)
+
+
+
+
+
